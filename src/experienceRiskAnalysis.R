23edc2308fa65d2c33b170621105124bb3ffddae
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2022-03-12 17:44:48 -0800

    Import data set

---
+++ src/experienceRiskAnalysis.R
@@ -0,0 +1,12 @@
+### Effect of experience on alicited risk
+### Dario Trujano-Ochoa
+
+
+# Data set ----
+
+ExperienceRisk_Sessions <- read.csv("Data/ExperienceRisk_Sessions.csv")
+Payoffs <- read.csv("Data/Payoffs.csv")
+
+## number of mistakes
+
+
