f7ee589622788fff5843d8ace6897fe1efdc02a5
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2022-03-22 16:07:24 -0700

    Prepare data with comments

--- src/experienceRiskAnalysis.R
+++ src/experienceRiskAnalysis.R
@@ -10,10 +10,11 @@ p_load(matrixStats)
 
 # Data set ----
 
-ExperienceRisk_Sessions <- read.csv("Data/ExperienceRisk_Sessions.csv")
-Payoffs <- read.csv("Data/Payoffs.csv")
-Payoffs <- Payoffs %>% mutate(Gamble = ï..Gamble,
-                              Exp_payoff = (A+B)/2)
+ExperienceRisk_Sessions <- read.csv("Data/ExperienceRisk_Sessions.csv", fileEncoding="UTF-8-BOM")
+Payoffs <- read.csv("Data/Payoffs.csv", fileEncoding="UTF-8-BOM")
+Payoffs <- Payoffs %>% mutate(Exp_payoff = (A+B)/2, 
+                              #Gamble = ï..Gamble # problem with windows
+                              )
 
 ## number of mistakes
 
@@ -23,11 +24,12 @@ gamble_payoff <- function(gamble,event) {
   return(g_payoff)
 }
 
-gamble_payoff(2,"o")
+# try the code
+# gamble_payoff(2,"o")
 
 ## correct payoffs written down by participants ----
 
-### first 12 elections (preselected) ----
+### first 12 elections (pre selected) ----
 sum_correct_payoffs_first <- rep(0,length(ExperienceRisk_Sessions$Session))
 for(p in 1:12){
   correct_payoff <- gamble_payoff(ExperienceRisk_Sessions[,paste("R",p,sep="")],
@@ -43,21 +45,25 @@ for(p in 1:12){
   sum_correct_payoffs_second <-  rowSums(cbind(correct_payoff,sum_correct_payoffs_second),na.rm = T)
 }
 
+# add sum of correct responses and create new useful data
 ExperienceRisk_Sessions <-
 ExperienceRisk_Sessions %>% mutate(sum_correct_payoffs_first = sum_correct_payoffs_first, 
                                   sum_correct_payoffs_second = sum_correct_payoffs_second,
                                   simple_diff = Gamble.2 - Gamble.1,
-                                  Session = ï..Session,
+                                  #Session = ï..Session, # problem in windows
                                   sum_correct_payoffs = sum_correct_payoffs_second+sum_correct_payoffs_first,
-                                  numE_all = numE_preselected+numE_freechoice,
-                                  Exp_Payoffs= Payoffs$Exp_payoff[match(Gamble.1,Payoffs$Gamble)])
+                                  # number of even events
+                                  numEven_all = numE_preselected+numE_freechoice,
+                                  Exp_Payoffs = Payoffs$Exp_payoff[match(Gamble.1,Payoffs$Gamble)])
 
 # Are people exploring?
 ExperienceRisk_Sessions <- 
 ExperienceRisk_Sessions %>% 
   mutate(Free_choice_var = rowVars(as.matrix(select(.data = ExperienceRisk_Sessions,F1:F12)),na.rm = T)) %>% 
   mutate(explore = Free_choice_var!=0)
+# consider selecting always the same choice in the free choice subsection as no exploration
 sum(ExperienceRisk_Sessions$explore)
+# 83 people out of 99 explore
 
 # Regressions ----
 
